bea9d98299d6cfa24c98f5bba36bbf9edac9a39a
Kanishk Pareek <kanishkgit@gmail.com>
2024-12-04 20:03:05 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 46065,  // Specify the port here
+  },
   proxy: {
     '/api': {
       target: 'https://api.ekqr.in',
